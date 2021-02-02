048932fae3004c8ce07f8c0ad198e8a71c98cb7f
Lucca Rizzi <luccarizzi29@gmail.com>
2021-02-02 21:12:23 +0000

    Within search-form.jsx, create a radio input for band, album and musician. Create the label and input text element for the Search Form. #1

--- client/pages/search-form.jsx
+++ client/pages/search-form.jsx
@@ -4,7 +4,53 @@ export default class SearchForm extends React.Component {
 
   render() {
     return (
-      <p>haya</p>
+      <form className='container mt-2'>
+        <div className='row'>
+          <div className='col d-flex justify-content-center mb-2'>
+            <div className="form-check form-check-inline">
+              <input
+              className="form-check-input"
+              type="radio"
+              name="radio-band"
+              id="radio-album"
+              value="band"></input>
+              <label
+              className="form-check-label"
+              htmlFor="radio-band">Band</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+              className="form-check-input"
+              type="radio"
+              name="radio-album"
+              id="radio-album"
+              value="album"></input>
+              <label
+              className="form-check-label"
+              htmlFor="radio-album">Album</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+              className="form-check-input"
+              type="radio"
+              name="radio-musician"
+              id="radio-musician"
+              value="musician"></input>
+              <label
+              className="form-check-label"
+              htmlFor="radio-musician">Musician</label>
+            </div>
+          </div>
+        </div>
+        <div className='col d-flex justify-content-center mb-2'>
+          <input
+          className=''
+          type='text'
+          name='input-search'
+          id='input-search'
+          placeholder='Search'></input>
+        </div>
+      </form>
     );
   }
 }
