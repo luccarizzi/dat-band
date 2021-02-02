0952c862028736c9b9161e00bfb19d93a94211be
Lucca Rizzi <luccarizzi29@gmail.com>
2021-02-02 19:04:08 +0000

    Within app.jsx, import SearchForm and return the SearchForm in the render method. #1

--- client/app.jsx
+++ client/app.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import Home from './pages/home';
+// import Home from './pages/home';
 import Navbar from './pages/navbar';
+import SearchForm from './pages/search-form';
 
 export default class App extends React.Component {
   render() {
     return (
       <>
         <Navbar />
-        <Home />
+        <SearchForm />
       </>
-    )
+    );
   }
 }
--- client/pages/search-form.jsx
+++ client/pages/search-form.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 export default class SearchForm extends React.Component {
 
   render() {
-    return
+    return (
+      <p>haya</p>
+    );
   }
 }
