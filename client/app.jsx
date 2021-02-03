a9bd08f42810061735cdd87fcdf20fdbba823d76
Lucca Rizzi <luccarizzi29@gmail.com>
2021-02-03 05:59:30 +0000

    Set the app.get endpoint to search the database using the category and search params. Setup the endpoint to check for and throw errors if necessary. #2

--- client/app.jsx
+++ client/app.jsx
@@ -11,7 +11,7 @@ export default class App extends React.Component {
   }
 
   getParameters(parameters) {
-    // console.log('Parameters', parameters)
+
   }
 
   render() {
--- server/index.js
+++ server/index.js
@@ -4,9 +4,61 @@ const staticMiddleware = require('./static-middleware');
 const ClientError = require('./client-error');
 const errorMiddleware = require('./error-middleware');
 
+const pg = require('pg');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
 const app = express();
 
+const jsonMiddleware = express.json();
+app.use(jsonMiddleware);
+
 app.use(staticMiddleware);
+
+app.get('/api', (req, res, next) => {
+  const { category, search } = req.body;
+  if (!category || !search) {
+    throw new ClientError(400, 'category and search are required fields');
+  }
+
+  const fixedSearch = `${search[0].toUpperCase()}${search.slice(1)}%`; // create function to solve all possible search input
+
+  let sql;
+  switch (category) {
+    case 'band':
+      sql = `
+        select "bandName", "debutYear"
+        from "bands"
+        where "bandName" like $1
+      `;
+      break;
+    case 'album':
+      sql = `
+        select "albumTitle", "releaseYear", "recordLabel", "albumImageUrl"
+        from "albums"
+        where "albumTitle" like $1
+      `;
+      break;
+    case 'musician':
+      sql = `
+        select "musicianFirstName", "musicianLastName", "dob", "musicianImageUrl"
+        from "musicians"
+        where "musicianFirstName" like $1
+        or    "musicianLastName"  like $1
+      `;
+      break;
+  }
+
+  const params = [fixedSearch];
+
+  db
+    .query(sql, params)
+    .then(result => res.json(result.rows))
+    .catch(err => next(err));
+
+});
 
 app.use(errorMiddleware);
 
