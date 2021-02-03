24c81f0080c6ef4366c854392b6107b319e378d0
Lucca Rizzi <luccarizzi29@gmail.com>
2021-02-03 02:23:18 +0000

    Create the error-middleware.js file and create (and export) the errorMiddleware function. Also require the ClientError. #2

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const ClientError = require('./client-error');
+const errorMiddleware = require('./error-middleware');
 
 const app = express();
 
 app.use(staticMiddleware);
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
