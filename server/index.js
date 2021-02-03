9ec4183b4290c937e33ba440a1c993f898abdcd6
Lucca Rizzi <luccarizzi29@gmail.com>
2021-02-03 02:17:49 +0000

    Create the client-error.jsx file and create (and export) the ClientError class. #2

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
+const ClientError = require('./client-error');
 
 const app = express();
 
