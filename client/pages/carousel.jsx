725c32be13d2fd4891038f61b0d476c79ed8d7e7
Lucca Rizzi <luccarizzi29@gmail.com>
2021-02-06 03:33:02 +0000

    Create a rightClick() method to show the previous image. #5

--- client/pages/carousel.jsx
+++ client/pages/carousel.jsx
@@ -9,7 +9,8 @@ export default class Carousel extends React.Component {
     this.componentDidMount = this.componentDidMount.bind(this);
     this.componentWillUnmount = this.componentWillUnmount.bind(this);
     this.timer = this.timer.bind(this);
-    this.leftArrow = this.leftArrow.bind(this);
+    this.leftClick = this.leftClick.bind(this);
+    this.rightClick = this.rightClick.bind(this);
   }
 
   componentDidMount() {
@@ -36,7 +37,7 @@ export default class Carousel extends React.Component {
     }
   }
 
-  leftArrow(event) {
+  leftClick(event) {
     const { imageIndex } = this.state;
     if (imageIndex === 0) {
       this.setState({
@@ -51,14 +52,29 @@ export default class Carousel extends React.Component {
     this.componentDidMount();
   }
 
+  rightClick(event) {
+    const { imageIndex } = this.state;
+    if (imageIndex === 2) {
+      this.setState({
+        imageIndex: 0
+      });
+      return;
+    }
+    this.setState({
+      imageIndex: imageIndex + 1
+    });
+    clearInterval(this.timerID);
+    this.componentDidMount();
+  }
+
   render() {
-    const { images } = this.props; // console.log(images)
+    const { images } = this.props;
     const { imageIndex } = this.state;
     return (
       <div>
-        <a><i onClick={this.leftArrow} className='fas fa-chevron-left position-absolute arrow left-arrow'></i></a>
+        <a><i onClick={this.leftClick} className='fas fa-chevron-left position-absolute arrow left-arrow'></i></a>
         <img className='img-fluid carousel-image' src={images[imageIndex].bandCarouselImageUrl}></img>
-        <a><i className='fas fa-chevron-right position-absolute arrow right-arrow'></i></a>
+        <a><i onClick={this.rightClick} className='fas fa-chevron-right position-absolute arrow right-arrow'></i></a>
       </div>
     );
   }
