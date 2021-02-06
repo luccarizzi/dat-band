8a0df5e1576712011d7e3dbbeb4b3204df5d8b06
Lucca Rizzi <luccarizzi29@gmail.com>
2021-02-06 03:27:10 +0000

    Create a leftClick() method to show the next image. #5

--- client/pages/carousel.jsx
+++ client/pages/carousel.jsx
@@ -9,6 +9,7 @@ export default class Carousel extends React.Component {
     this.componentDidMount = this.componentDidMount.bind(this);
     this.componentWillUnmount = this.componentWillUnmount.bind(this);
     this.timer = this.timer.bind(this);
+    this.leftArrow = this.leftArrow.bind(this);
   }
 
   componentDidMount() {
@@ -35,12 +36,27 @@ export default class Carousel extends React.Component {
     }
   }
 
+  leftArrow(event) {
+    const { imageIndex } = this.state;
+    if (imageIndex === 0) {
+      this.setState({
+        imageIndex: 2
+      });
+      return;
+    }
+    this.setState({
+      imageIndex: imageIndex - 1
+    });
+    clearInterval(this.timerID);
+    this.componentDidMount();
+  }
+
   render() {
     const { images } = this.props; // console.log(images)
     const { imageIndex } = this.state;
     return (
       <div>
-        <a><i className='fas fa-chevron-left position-absolute arrow left-arrow'></i></a>
+        <a><i onClick={this.leftArrow} className='fas fa-chevron-left position-absolute arrow left-arrow'></i></a>
         <img className='img-fluid carousel-image' src={images[imageIndex].bandCarouselImageUrl}></img>
         <a><i className='fas fa-chevron-right position-absolute arrow right-arrow'></i></a>
       </div>
