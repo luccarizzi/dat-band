17893cd2f04234556e99fda080074296a1889747
Lucca Rizzi <luccarizzi29@gmail.com>
2021-02-06 02:30:40 +0000

    Set the component's state object with the imageIndex property. #5

--- client/pages/carousel.jsx
+++ client/pages/carousel.jsx
@@ -1,16 +1,17 @@
-import { extend } from 'lodash';
 import React from 'react';
 
 export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      imageIndex: 0
+    };
   }
 
   render() {
     // console.log(this.props)
     return (
       <h1>test</h1>
-    )
+    );
   }
 }
