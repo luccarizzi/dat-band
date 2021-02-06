eb1563bae05e423a75be3028d487888cba4832ea
Lucca Rizzi <luccarizzi29@gmail.com>
2021-02-06 02:17:15 +0000

    Create a Carousel component. #5

---
+++ client/pages/carousel.jsx
@@ -0,0 +1,15 @@
+import { extend } from 'lodash';
+import React from 'react';
+
+export default class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <h1>test</h1>
+    )
+  }
+}
