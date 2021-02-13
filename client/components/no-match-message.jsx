022d3993c34e0c206e5aa7242c1af51847a2a55b
Lucca Rizzi <luccarizzi29@gmail.com>
2021-02-13 21:31:53 +0000

    Refactor the message components to be responsive. #25

--- client/components/no-match-message.jsx
+++ client/components/no-match-message.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function NoMatchMessage(props) {
   const { search, category } = props.result;
   return (
-    <div className='row'>
-      <div className='col my-2'>
+    <div className='row justify-content-center'>
+      <div className='col-12 col-lg-8 my-2'>
         <p className='alert alert-warning p-1'>Sorry, no {category}s found for &apos;{search}&apos;.</p>
       </div>
     </div>
