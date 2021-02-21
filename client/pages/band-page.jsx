f4b9a3fa74440436d8b44a0b07144c0e703ce130
Lucca Rizzi <luccarizzi29@gmail.com>
2021-02-21 01:57:38 +0000

    Create a NotFound component so the user will know that the requested page doesn't exist. #34

--- client/pages/band-page.jsx
+++ client/pages/band-page.jsx
@@ -6,6 +6,7 @@ import BandMembers from '../components/band/band-members';
 import BandDiscography from '../components/band/band-discography';
 import BandVideography from '../components/band/band-videography';
 import BandSeeAlso from '../components/band/band-see-also';
+import NotFound from '../components/not-found';
 
 export default class BandPage extends React.Component {
   constructor(props) {
@@ -48,7 +49,7 @@ export default class BandPage extends React.Component {
     const { imageCarousel, title, info, members, discography, videography, genre } = data;
     // if (data.error) {
     //   return (
-    //     <p>error</p>
+    //     <NotFound />
     //   )
     // }
 
