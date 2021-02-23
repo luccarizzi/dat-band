8d053146a60fa872a753815678e7099df1ef299f
Lucca Rizzi <luccarizzi29@gmail.com>
2021-02-23 22:38:26 +0000

    Add / to all url for the fetch requests. #36

--- client/components/band/band-image-carousel.jsx
+++ client/components/band/band-image-carousel.jsx
@@ -9,8 +9,6 @@ export default class BandImageCarousel extends React.Component {
     this.componentDidMount = this.componentDidMount.bind(this);
     this.componentWillUnmount = this.componentWillUnmount.bind(this);
     this.timer = this.timer.bind(this);
-    this.leftClick = this.leftClick.bind(this);
-    this.rightClick = this.rightClick.bind(this);
   }
 
   componentDidMount() {
@@ -37,45 +35,17 @@ export default class BandImageCarousel extends React.Component {
     }
   }
 
-  leftClick(event) {
-    const { imageIndex } = this.state;
-    if (imageIndex === 0) {
-      this.setState({
-        imageIndex: 2
-      });
-      return;
-    }
-    this.setState({
-      imageIndex: imageIndex - 1
-    });
-    clearInterval(this.timerID);
-    this.componentDidMount();
-  }
-
-  rightClick(event) {
-    const { imageIndex } = this.state;
-    if (imageIndex === 2) {
-      this.setState({
-        imageIndex: 0
-      });
-      return;
-    }
-    this.setState({
-      imageIndex: imageIndex + 1
-    });
-    clearInterval(this.timerID);
-    this.componentDidMount();
-  }
-
   render() {
     const { bandData } = this.props;
     const { imageIndex } = this.state;
     return (
       <div className='row g-0'>
         <div className='col d-flex justify-content-center mb-2'>
-          {/* <a><i onClick={this.leftClick} className='fas fa-chevron-left arrows'></i></a> */}
-          <img className='object-fit-cover carousel-image rounded' src={bandData[imageIndex].bandCarouselImageUrl} alt={`${bandData[imageIndex].bandName} band members.`}></img>
-          {/* <a><i onClick={this.rightClick} className='fas fa-chevron-right arrows'></i></a> */}
+          <img
+            className='object-fit-cover carousel-image rounded'
+            src={bandData[imageIndex].bandCarouselImageUrl}
+            alt={`${bandData[imageIndex].bandName} band members.`}>
+          </img>
         </div>
       </div>
     );
